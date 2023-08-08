fe784065b15eca8a7fe8b3c356a4412c61314b4e
Julia Novikova <julikova.n@gmail.com>
2023-08-08 23:04:00 +0300

    fix Test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -12,25 +12,27 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
+import org.openqa.selenium.Keys;
+
 public class CardDeliveryTest {
 
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
+    String planningDate = generateDate(5, "dd.MM.yyy");
     @Test
     void shouldRegistered() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        String currentDate = generateDate(5, "dd.MM.yyy");
-        $("[data-test-id='date'] input").setValue(currentDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Новикова Юлия");
         $("[data-test-id='phone'] input").setValue("+79169930331");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']")
                 .shouldBe(Condition.visible, Duration.ofSeconds(500))
-                .shouldHave(Condition.exactText("Ваша встреча успешно забронирована на " + currentDate));
+                .shouldHave(Condition.exactText("Ваша встреча успешно забронирована на " + planningDate));
     }
 }
