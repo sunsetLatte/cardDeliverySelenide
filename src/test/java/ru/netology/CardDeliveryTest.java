e170bce7d76270b51aa3daf7b2ad9e67e16d7fab
Julia Novikova <julikova.n@gmail.com>
2023-08-16 23:09:35 +0300

    add new Tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -19,10 +19,12 @@ public class CardDeliveryTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     String planningDate = generateDate(5, "dd.MM.yyy");
-    @Test
+
+    @Test                     // Позитивный тест
     void shouldRegistered() {
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -31,8 +33,57 @@ public class CardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+79169930331");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id='notification']")
-                .shouldBe(Condition.visible, Duration.ofSeconds(500))
-                .shouldHave(Condition.exactText("Ваша встреча успешно забронирована на " + planningDate));
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(50));
+        $("[class='notification__content']")
+                .shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
+    @Test                // Негативный тест, невалидный город
+    void InvalidCity() {
+//        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Moscow");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Новикова Юлия");
+        $("[data-test-id='phone'] input").setValue("+79169930331");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Доставка в выбранный город недоступна"))
+                .shouldBe(visible)
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test                // Негативный тест, невалидное имя
+    void InvalidName() {
+//        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Novikova Julia");
+        $("[data-test-id='phone'] input").setValue("+79169930331");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Имя и Фамилия указаные неверно"))
+                .shouldBe(visible)
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test                 // Негативный тест, невалидный телефон
+    void InvalidPhone() {
+//        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Новикова Юлия");
+        $("[data-test-id='phone'] input").setValue("+791699300331");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Телефон указан неверно"))
+                .shouldBe(visible)
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
